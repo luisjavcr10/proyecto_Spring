33f1fd73b25372c61d50ff516b2d1b937a296283
Luis Javier Castillo Rabanal <luisjaviercastillorabanal@MacBook-Air-de-Luis.local>
2024-10-18 12:39:39 -0500

    Inicio del proyecto

---
+++ src/main/java/com/luiscastillo/market/LuiscastilloMarketApplication.java
@@ -0,0 +1,13 @@
+package com.luiscastillo.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuiscastilloMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuiscastilloMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luiscastillo/market/LuiscastilloMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiscastillo.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuiscastilloMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
